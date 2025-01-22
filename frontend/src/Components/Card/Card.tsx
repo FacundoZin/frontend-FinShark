10a47f77216d8a2e87c807762ad02d818ccdbf17
facu <facundozin10@gmail.com>
2025-01-22 16:15:13 -0300

    lalalalal

---
+++ frontend/src/Components/Card/Card.tsx
@@ -0,0 +1,15 @@
+type Props = {}
+const card = (props: Props) => {
+  return (
+    <div className="card">
+        <img src="https://ar.pinterest.com/pin/985231163492840/" alt="Image" />
+        <div className="details">
+            <h2>AAPL</h2>
+            <p>$100</p>
+        </div>
+        <p className="infon">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus porro distinctio quibusdam velit exercitationem, ea aperiam quod optio expedita rerum laboriosam soluta, dicta possimus repellendus? Accusamus facilis nisi id consequuntur!</p>
+    </div>
+
+  )
+}
+export default card
